1fe3babcf23e990d396f0e23efd85bcf4ef5e7ab
Debasis-pramanik2000 <pramanikdebasis2003@gmail.com>
2026-09-21 07:09:54 +0530

    hello

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use('/api', limiter);
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:5000'
+    'https://scholl-frontend-new.vercel.app/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
